1e5dc01effeb5af1b22db67bb6f4522270f244a8
copylife <llk199626@163.com>
2020-10-30 23:48:00 +0800

    Linked List sort , push , __len__， append method

--- linked_list/single.py
+++ linked_list/single.py
@@ -1,4 +1,48 @@
-from typing import Any, Optional, List
+from abc import abstractmethod
+from typing import Any, Optional, List, Callable, runtime_checkable, Protocol, Union
+
+
+@runtime_checkable
+class SupportsGt(Protocol):
+    """An ABC with one abstract method __complex__."""
+    __slots__ = ()
+
+    @abstractmethod
+    def __gt__(self, other) -> bool:
+        pass
+
+
+@runtime_checkable
+class SupportsLt(Protocol):
+    """An ABC with one abstract method __complex__."""
+    __slots__ = ()
+
+    @abstractmethod
+    def __lt__(self, other) -> bool:
+        pass
+
+
+@runtime_checkable
+class SupportsGe(Protocol):
+    """An ABC with one abstract method __complex__."""
+    __slots__ = ()
+
+    @abstractmethod
+    def __ge__(self, other) -> bool:
+        pass
+
+
+@runtime_checkable
+class SupportsLe(Protocol):
+    """An ABC with one abstract method __complex__."""
+    __slots__ = ()
+
+    @abstractmethod
+    def __le__(self, other) -> bool:
+        pass
+
+
+SupportsCompare = Union[SupportsGe, SupportsGt, SupportsLe, SupportsLt]
 
 
 class EmptyError(Exception):
@@ -15,6 +59,11 @@ class _Node:
     def __init__(self, element: Any, next_node: Optional['_Node']):
         self.element = element
         self.next_node = next_node
+
+    def __repr__(self):
+        return str(self.element)
+
+    __str__ = __repr__
 
 
 class SinglyLinkedStack:
@@ -191,42 +240,138 @@ class CircularQueue:
     __str__ = __repr__
 
 
-if __name__ == '__main__':
-    c = SinglyLinkedStack()
-    c.push(5)
-    c.push(6)
-    c.push(7)
-    print('Linked Stack:', c)
-    c.top()
-    c.pop()
-    print('Linked Stack:', c)
-    d = SinglyLinkedQueue(3)
-    d.enqueue(4)
-    d.enqueue(5)
-    d.enqueue(6)
-    print('Linked Queue:', d)
-    try:
-        d.enqueue(7)
-    except SizeLimitError:
-        pass
-    d.dequeue()
-    print('Linked Queue:', d)
+class LinkedList:
 
-    f = CircularQueue(3)
-    f.enqueue(1)
-    f.enqueue(2)
-    f.enqueue(3)
-    print('Circular Queue:', f)
-    try:
-        f.enqueue(4)
-    except SizeLimitError:
-        pass
-    f.dequeue()
-    print('Circular Queue:', f)
-    f.enqueue(4)
-    print('Circular Queue:', f)
-    f.rotate()
-    print('Circular Queue:', f)
-    f.dequeue()
-    f.dequeue()
-    print('Circular Queue:', f)
+    def __init__(self):
+        self._head: Optional['_Node'] = None
+        self._tail: Optional['_Node'] = None
+        self._size: int = 0
+
+    def __repr__(self):
+        if not self._size:
+            return ''
+        elements = []
+        node = self._head
+        while node:
+            elements.append(str(node.element))
+            node = node.next_node
+        return ' -> '.join(elements)
+
+    __str__ = __repr__
+
+    def append(self, e: Any):
+        node = _Node(e, None)
+        if self._size == 0:
+            self._head = node
+        else:
+            self._tail.next_node = node
+        self._tail = node
+        self._size += 1
+
+    def push(self, e: Any) -> None:
+        node = _Node(e, None)
+        if self._size == 0:
+            self._tail = node
+        else:
+            node.next_node = self._head
+        self._head = node
+        self._size += 1
+
+    def exist(self, e: Any) -> bool:
+        if not self._size:
+            return False
+        node = self._head
+        while node:
+            if node.element == e:
+                return True
+            node = node.next_node
+        return False
+
+    def sort(self, key: Callable[[Any], SupportsCompare] = None, reverse: bool = True) -> None:
+        if key is None:
+            key = lambda x: x
+        if self._size <= 1:
+            return
+
+        i = 0
+        while i < self._size - 1:
+            prev = None
+            current = self._head
+            j = 0
+            while j < self._size - 1 - i:
+                if not current.next_node:
+                    break
+                next = current.next_node
+                if (reverse and (key(current.element) < key(next.element))) or (
+                        (not reverse) and (key(current.element) > key(next.element))):
+                    if j == 0:
+                        self._head = next
+                    else:
+                        prev.next_node = next
+                    current.next_node = next.next_node
+                    prev = next
+                    next.next_node = current
+                else:
+                    prev = current
+                    current = next
+                j += 1
+
+            if i == 0:
+                self._tail = current
+            i += 1
+
+    def __len__(self):
+        return self._size
+
+
+if __name__ == '__main__':
+    # c = SinglyLinkedStack()
+    # c.push(5)
+    # c.push(6)
+    # c.push(7)
+    # print('Linked Stack:', c)
+    # c.top()
+    # c.pop()
+    # print('Linked Stack:', c)
+    # d = SinglyLinkedQueue(3)
+    # d.enqueue(4)
+    # d.enqueue(5)
+    # d.enqueue(6)
+    # print('Linked Queue:', d)
+    # try:
+    #     d.enqueue(7)
+    # except SizeLimitError:
+    #     pass
+    # d.dequeue()
+    # print('Linked Queue:', d)
+    #
+    # f = CircularQueue(3)
+    # f.enqueue(1)
+    # f.enqueue(2)
+    # f.enqueue(3)
+    # print('Circular Queue:', f)
+    # try:
+    #     f.enqueue(4)
+    # except SizeLimitError:
+    #     pass
+    # f.dequeue()
+    # print('Circular Queue:', f)
+    # f.enqueue(4)
+    # print('Circular Queue:', f)
+    # f.rotate()
+    # print('Circular Queue:', f)
+    # f.dequeue()
+    # f.dequeue()
+    # print('Circular Queue:', f)
+    f = LinkedList()
+    f.append(3)
+    f.append(6)
+    f.append(8)
+    f.append(1)
+    f.append(9)
+    f.push(32)
+    print(f)
+    f.sort()
+    print(f)
+    f.sort(reverse=False)
+    print(f)
